69ac074da370115465894c7761064830aa686740
polarizer45lc <m1873.45lc@gmail.com>
2018-09-14 15:15:16 +0900

    main_pre.view

--- app/assets/javascripts/pre_view.js
+++ app/assets/javascripts/pre_view.js
@@ -1,38 +1,35 @@
-$(document).ready(function(){
-  $("#main_image_uploader").change(function(){
-    var src = $(this).children('#main_preview').attr('src');
-    console.log(src);
-    alert(src);
+$(function(){
+  $('#main_image_uploader').change(function(e){
+    var file = e.target.files[0];
+    var reader = new FileReader();
+    if(file.type.indexOf("image") < 0){
+      alert("画像ファイルを指定してください。");
+      return false;
+    }
+    reader.onload = (function(file){
+      return function(e){
+        $("#main_preview").attr("src", e.target.result);
+        $("#main_preview").attr("title", file.name);
+      };
+    })(file);
+    reader.readAsDataURL(file);
   });
 });
 
-
-
-
-
-
-
-
-
-// $(function(){
-//   $('#main_image_uploader').change(function(e){
-//     //ファイルオブジェクトを取得する
-//     var file = e.target.files[0];
-//     var reader = new FileReader();
-
-//     //画像でない場合は処理終了
-//     if(file.type.indexOf("image") < 0){
-//       alert("画像ファイルを指定してください。");
-//       return false;
-//     }
-
-//     //アップロードした画像を設定する
-//     reader.onload = (function(file){
-//       return function(e){
-//         $("#prototype_captured_images_attributes_0_status").attr("src", e.target.result);
-//         $("#prototype_captured_images_attributes_0_status").attr("title", file.name);
-//       };
-//     })(file);
-//     reader.readAsDataURL(file);
-//   });
-// });
+$(function(){
+  $('#sub_image_uploader').change(function(e){
+    var file = e.target.files[0];
+    var reader = new FileReader();
+    if(file.type.indexOf("image") < 0){
+      alert("画像ファイルを指定してください。");
+      return false;
+    }
+    reader.onload = (function(file){
+      return function(e){
+        $("#sub_preview").attr("src", e.target.result);
+        $("#sub_preview").attr("title", file.name);
+      };
+    })(file);
+    reader.readAsDataURL(file);
+  });
+});
