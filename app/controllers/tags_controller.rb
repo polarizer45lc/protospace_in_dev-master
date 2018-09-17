9529a407ef90592897800b6d3b0c10361b70ac75
mochilefty <mochi.lefty@gmail.com>
2018-09-17 14:14:18 +0900

    Edit Show, tags_controller

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,10 +1,10 @@
 class TagsController < ApplicationController
-
   def index
     @tags = Tag.order('created_at DESC')
   end
 
   def show
+    tag= Tag.find(params[:id])
+    @prototypes = tag.prototypes.page(params[:page]).per(5)
   end
-
 end
