4825144f2765e8cdca9dceac58bf50e1a785fcc4
fumiya0312 <good.job0312@gmail.com>
2018-09-22 10:49:37 +0900

    Like controller
    Like controller設定

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,31 @@
 class LikesController < ApplicationController
+
+  def create
+    @prototype = Prototype.find(params[:prototype_id])
+    unless @mprototype.like?(current_user)
+      @prototype.like(current_user)
+      @prototype.reload
+      respond_to do |format|
+        format.html { redirect_to request.referrer || root_url }
+        # format.html { redirect_to prototypes_path(@prototype) }
+        format.json
+      end
+    end
+  end
+
+  def destroy
+    @prototype = Like.find(params[:id]).prototype
+    if @prototype.like?(current_user)
+      @prototype.unlike(current_user)
+      # @prototype.reload
+      respond_to do |format|
+        format.html { redirect_to request.referrer || root_url }
+        # format.html { redirect_to prototypes_path(@prototype) }
+        # prototype_path
+        format.json
+      end
+    end
+  end
 end
+
+
